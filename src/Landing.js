2e4bcb2b08d4c987ba80c450cacdcc58ace91cc4
Ike Lyons <lyonsw@rpi.edu>
2022-04-26 23:09:50 -0400

    Changed positiong and fixed import bug

--- src/Landing.js
+++ src/Landing.js
@@ -4,11 +4,11 @@ import "./Landing.css";
 function Landing() {
   return (
     <div className="container">
-      <Parallax translateY={[100, -100]} translateX={[-100, 100]} className="lower">
+      <Parallax translateY={[100, -100]} translateX={[-100, 100]} className="box">
         <div className="blackBox">test</div>
       </Parallax>
-      <Parallax className="text" translateY={[-70, 70]} translateX={[130, -130]}>
-        <span className="h1">Ike Lyons</span>
+      <Parallax translateY={[-70, 70]} translateX={[-130, 130]} className="name">
+        <span className="text">Ike Lyons</span>
       </Parallax>
     </div>
   );
