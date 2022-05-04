78a75d70f4c783e73e8527b93b9860ca2e5458c5
Ike Lyons <lyonsw@rpi.edu>
2022-05-04 16:41:16 -0400

    added downward scroll prompt

--- src/Landing.js
+++ src/Landing.js
@@ -7,7 +7,7 @@ function Landing() {
   return (
     <div className="root">
       <div className="nameHeader">
-        <Parallax translateY={[150, -150]}>
+        <Parallax translateY={[200, -200]}>
           <span className="text">i'm ike lyons</span>
         </Parallax>
       </div>
@@ -17,6 +17,21 @@ function Landing() {
             {word}
           </Parallax>
         ))}
+      </div>
+      <div className="downarrows">
+        <svg style={{ color: "#ffffff" }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi" viewBox="0 0 16 16">
+          {" "}
+          <path
+            fillRule="evenodd"
+            d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+            fill="white"
+          ></path>{" "}
+          <path
+            fillRule="evenodd"
+            d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+            fill="white"
+          ></path>{" "}
+        </svg>
       </div>
     </div>
   );
