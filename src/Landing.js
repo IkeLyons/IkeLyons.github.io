e08f35a38f5962c2ff9440b0ab69025e711601cc
Ike Lyons <lyonsw@rpi.edu>
2022-05-04 16:16:57 -0400

    look like an actual homepage

--- src/Landing.js
+++ src/Landing.js
@@ -1,19 +1,22 @@
 import { Parallax } from "react-scroll-parallax";
 import "./Landing.css";
 
+const words = "nice to meet you".split("");
+
 function Landing() {
   return (
     <div className="root">
       <div className="nameHeader">
-        <span className="text">Ike Lyons</span>
+        <Parallax translateY={[150, -150]}>
+          <span className="text">i'm ike lyons</span>
+        </Parallax>
       </div>
       <div className="container">
-        <Parallax translateY={[100, -100]} translateX={[-100, 100]} className="box">
-          <div className="blackBox"></div>
-        </Parallax>
-        <Parallax translateY={[-70, 70]} translateX={[-130, 130]} className="name">
-          <span className="text">Ike Lyons</span>
-        </Parallax>
+        {words.map((word, i) => (
+          <Parallax key={i} translateX={[100 * (i - 7.5) * -1, 100 * (i - 7.5)]} className="word">
+            {word}
+          </Parallax>
+        ))}
       </div>
     </div>
   );
