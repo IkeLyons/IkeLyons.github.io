82ef400adde12341c847a52fe3dc1e057d99b1d5
Ike Lyons <lyonsw@rpi.edu>
2022-05-26 00:20:20 -0400

    QOL and visual changes

--- src/Cursor.js
+++ src/Cursor.js
@@ -4,11 +4,20 @@ import "./Cursor.css";
 function Cursor() {
   const [pos, setPos] = useState({ x: 0, y: 0 });
   const [visible, setVisible] = useState(true);
+  const [linkHovered, setLinkHovered] = useState(false);
 
   useEffect(() => {
     addEventListeners();
+    handleLinkHoverEvents();
     return () => removeEventListeners();
   }, []);
+
+  const handleLinkHoverEvents = () => {
+    document.querySelectorAll("a").forEach((el) => {
+      el.addEventListener("mouseover", () => setLinkHovered(true));
+      el.addEventListener("mouseout", () => setLinkHovered(false));
+    });
+  };
 
   const addEventListeners = () => {
     document.addEventListener("mousemove", onMouseMove);
@@ -38,7 +47,8 @@ function Cursor() {
     <div>
       {visible ? (
         <div
-          className="cursor"
+          //   className={"cursor" + linkHovered ? " cursorHovered" : ""}
+          className={`cursor ${linkHovered ? "cursorHovered" : ""}`}
           style={{
             left: `${pos.x}px`,
             top: `${pos.y}px`,
@@ -46,14 +56,6 @@ function Cursor() {
         />
       ) : null}
     </div>
-
-    // <svg xmlns="<http://www.w3.org/2000/svg>">
-    //     <path
-    //       strokeWidth="2"
-    //       stroke="white"
-    //       d="M59.427,50.819 Q60,50 60.000,49.000L60.000,41.000 Q60,40 59.427,39.181L53.573,30.819 Q53,30 52.000,30.000L38.000,30.000 Q37,30 36.427,30.819L30.573,39.181 Q30,40 30.000,41.000L30.000,49.000 Q30,50 30.573,50.819L36.427,59.181 Q37,60 38.000,60.000L52.000,60.000 Q53,60 53.573,59.181Z"
-    //     />
-    //   </svg>
   );
 }
 
