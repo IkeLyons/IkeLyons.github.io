fb41fa1e53b91302f141d9b621d079c31843690a
Ike Lyons <lyonsw@rpi.edu>
2022-05-30 15:43:31 -0400

    added project descriptions

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -9,14 +9,17 @@ function Projects() {
           <img src="/a2pats.png" alt="A2PATS Logo"></img>
         </a>
         <p className="projectDescription">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          During the spring of 2021 my project was textron's a2pats electromagnetic warfare simulation software. there i implemented a distributed simulation
+          standard (dis) across the full-stack using c# and wpf .net. This allowed our simultion software to seemlessly integrate with other (often proprietary)
+          warfare simulation software. I also interfaced with Boeing Company to troubleshoot issues and ensure end-user satisfaction. Click the logo to view the
+          website!
         </p>
       </Parallax>
       <Parallax className="project" translateX={[100, 0]} shouldAlwaysCompleteAnimation={true}>
         <p className="projectDescription">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum id venenatis a
-          condimentum vitae sapien pellentesque habitant. Blandit turpis cursus in hac habitasse platea dictumst quisque sagittis.
+          During the fall of 2020 I was a project lead for a Rensselaer Center for Open Source project called yacs. This website is our schools de-facto course
+          scheduler, and during my tenure we finished out a full re-design and modernization of the site in vue.js and flask; creating the site you see today.
+          Click the logo to view the website!
         </p>
         <a className="projectVisual" href="https://yacs.cs.rpi.edu">
           <img src="/yacstext.png" alt="YACS Logo"></img>
@@ -27,10 +30,10 @@ function Projects() {
           <img src="/ACSIA.png" alt="ACSIA Logo"></img>
         </a>
         <p className="projectDescription">
-          Elit sed vulputate mi sit amet mauris commodo quis imperdiet. Cursus euismod quis viverra nibh cras. Vitae nunc sed velit dignissim sodales ut eu sem
-          integer. A scelerisque purus semper eget. Dapibus ultrices in iaculis nunc sed augue lacus. Vitae congue eu consequat ac felis. Nisl nisi scelerisque
-          eu ultrices vitae auctor eu augue ut. Non nisi est sit amet facilisis magna. Vel pretium lectus quam id. Quam lacus suspendisse faucibus interdum
-          posuere lorem ipsum.
+          I currently volunteer for acsia where I aid research efforts, manage the website, and support other researchers through infrastructure creation.
+          Founder Zach Hadley calls me the "data og" due to my efforts in dataset collection/web scraping as well as data visualization across multiple research
+          projects. I'm also looking at using GitHub's social impact initiative to help other non-profits in their work and to increase the public ability to
+          interact with acsia's research. Click the logo to view the website!
         </p>
       </Parallax>
     </div>
